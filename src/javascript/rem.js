e52daab1a94c98b394e1059925a2baf281451a0d
Kingsley-Don <tj971204@gmail.com>
2016-10-06 01:32:59 +0800

    zoom

--- src/javascript/rem.js
+++ src/javascript/rem.js
@@ -1,4 +1,4 @@
 var dpr = window.devicePixelRatio;
-document.write('<meta name="viewport" content="width=device-width,initial-scale=' + 1 / dpr +',minimum-scale=' + 1 / dpr +',maximum-scale=' + 1 / dpr +',user-scalable=no" />')
+document.write('<meta name="viewport" content="width=device-width,initial-scale=' + 1 / dpr +',minimum-scale=' + 1 / dpr +',maximum-scale=' + 1 / dpr +',user-scalable=0" />')
 var clientWidth = document.documentElement.clientWidth;
 document.getElementsByTagName('html')[0].style.fontSize = clientWidth / 10 + 'px';
