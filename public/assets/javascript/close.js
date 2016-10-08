ae6caab2c0833f6720a86fc1cb09bd4954e36801
Kingsley-Don <tj971204@gmail.com>
2016-10-09 00:12:09 +0800

    board-one

--- public/assets/javascript/close.js
+++ public/assets/javascript/close.js
@@ -1,1 +1,1 @@
-function remove(){document.getElementById("app").style.display="none"}
+function closeapp(){document.getElementById("app").style.display="none",document.getElementById("scroll").style.marginTop="1.40625rem"}
--- src/javascript/close.js
+++ src/javascript/close.js
@@ -1,3 +1,4 @@
-function remove(){
+function closeapp(){
 	document.getElementById("app").style.display="none";
+	document.getElementById("scroll").style.marginTop="1.40625rem";
 }
