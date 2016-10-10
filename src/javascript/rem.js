d438d349a3e0808ea2dff9ada7293a133a947c6f
Kingsley-Don <tj971204@gmail.com>
2016-10-10 13:14:00 +0800

    addreadme

--- src/javascript/rem.js
+++ src/javascript/rem.js
@@ -1,4 +1,10 @@
-var dpr = window.devicePixelRatio;
-document.getElementsByTagName('head')[0].insertAdjacentHTML('beforeend','<meta name="viewport" content="width=device-width,initial-scale=' + 1 / dpr +',minimum-scale=' + 1 / dpr +',maximum-scale=' + 1 / dpr +',user-scalable=no" />');
-var clientWidth = document.documentElement.clientWidth;
-document.getElementsByTagName('html')[0].style.fontSize = clientWidth / 10 + 'px';
+function dprfunc(){
+	var dpr = window.devicePixelRatio;
+	document.getElementsByTagName('head')[0].insertAdjacentHTML('beforeend','<meta name="viewport" content="width=device-width,initial-scale=' + 1 / dpr +',minimum-scale=' + 1 / dpr +',maximum-scale=' + 1 / dpr +',user-scalable=no" />');
+}
+function width() {
+	var clientWidth = document.documentElement.clientWidth;
+	document.getElementsByTagName('html')[0].style.fontSize = clientWidth / 10 + 'px';
+}
+dprfunc();
+width();
